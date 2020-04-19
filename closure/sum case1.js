7e3cc02280bb70dec391a25bc8814d63888ce27c
abhishekthakurak <abhishekthakurak28@gmail.com>
2020-04-19 13:44:21 +0530

    currying

---
+++ closure/sum case1.js
@@ -0,0 +1,22 @@
+/**
+ * Create a sum function which can do the following
+ * 
+ * sum(1) // print 1
+ * sum(2) // sum(2) in next line will print 3
+ * sum(30) // sum(30) in next line will print 33
+ * 
+ */
+
+function sumWrapper () {
+    let total = 0;
+    return function (adder) {
+        total += adder
+        return total
+    }
+}
+
+const sum = sumWrapper()
+
+console.log(sum(1))
+console.log(sum(2))
+console.log(sum(30))
--- currying/sum(1)(2,3)fixedArgCount.js
+++ currying/sum(1)(2,3)fixedArgCount.js
@@ -4,8 +4,10 @@ function add (total) {
       function resultFn() {
           args = args.concat(Array.prototype.slice.apply(arguments));
           if (args.length>=total) {
-                return args.slice(0,total).reduce(function(acc,next){ 
-                    return acc+next},0); //will only sum first 3 arguments
+                return args.slice(0,total).reduce(
+                    function (acc , next) { 
+                    return acc+next
+                },0); //will only sum first {total} arguments
           }
           return resultFn;
       }
