55ea68d5b69f9fcffe2ea728d938a3ff8614079b
abhishekthakurak <abhishekthakurak28@gmail.com>
2020-04-19 13:30:32 +0530

    sum currying added

---
+++ currying/sum(1)(2,3)(4).js
@@ -0,0 +1,23 @@
+function add (total) {
+    function recursor(){
+      let args = Array.prototype.slice.apply(arguments);
+      function resultFn() {
+          args = args.concat(Array.prototype.slice.apply(arguments));
+          if (args.length>=total) {
+                return args.slice(0,total).reduce(function(acc,next){ 
+                    return acc+next},0); //will only sum first 3 arguments
+          }
+          return resultFn;
+      }
+      return resultFn();
+    }
+    return recursor
+  }
+  
+  let myAdd = add(3)
+  myAdd(1, 3, 5)
+  myAdd(1, 2)(4)
+  myAdd(1)(2, 5)
+  myAdd(1)(2)(2)
+
+  
