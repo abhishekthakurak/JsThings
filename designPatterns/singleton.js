8a77c173ec516ee0b07a5eecb350f63350dd15c6
abhishekthakurak <abhishekthakurak28@gmail.com>
2020-07-23 09:45:16 +0530

    some design patterns

---
+++ designPatterns/singleton.js
@@ -0,0 +1,33 @@
+var mySingleton = (function () {
+    // Instance stores a reference to the Singleton
+    var instance;
+    function init() {
+      // Singleton
+      // Private methods and variables
+      function privateMethod(){
+          console.log( "I am private" );
+      }
+      var privateVariable = "Im also private";
+      var privateRandomNumber = Math.random();
+      return {
+        // Public methods and variables
+        publicMethod: function () {
+          console.log( "The public can see me!" );
+        },
+        publicProperty: "I am also public",
+        getRandomNumber: function() {
+          return privateRandomNumber;
+        }
+      };
+    };
+    return {
+      // Get the Singleton instance if one exists
+      // or create one if it doesn't
+      getInstance: function () {
+        if ( !instance ) {
+          instance = init();
+        }
+        return instance;
+      }
+    };
+  })();
